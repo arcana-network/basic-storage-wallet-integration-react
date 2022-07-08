014ff78a6e6187133e386238bee043595bf1ff80
mmjee <root@git.maharshi.ninja>
2022-07-08 11:53:43 +0530

    Update SDKs and fix errors

--- src/lib/storageProvider.js
+++ src/lib/storageProvider.js
@@ -1,22 +1,19 @@
 import { StorageProvider } from '@arcana/storage'
-import { WalletProvider, AppMode } from '@arcana/wallet'
+import { AuthProvider, AppMode } from '@arcana/auth'
 
 let storageProvider
 
 export async function getWalletInstance () {
-  const wallet = new WalletProvider({
-    appId: process.env.REACT_APP_ARCANA_APP_ID,
-    inpageProvider: false /* sets window.arcana.provider and tries to set window.ethereum to the provider */
-  })
+  const wallet = new AuthProvider(process.env.REACT_APP_ARCANA_APP_ID)
   await wallet.init({ appMode: AppMode.Full, position: 'right' })
-  return wallet.getProvider()
+  return wallet.provider
 }
 
 export default async function getStorageProvider (provider) {
   if (!storageProvider) {
     storageProvider = new StorageProvider({
       appId: process.env.REACT_APP_ARCANA_APP_ID,
-      gateway: 'https://gateway-dev.arcana.network/api/v1/',
+      // gateway: 'https://gateway-dev.arcana.network/api/v1/',
       provider
     })
   }
