a32a4fc19773f084a7bfad926e2affa7a318487d
makylfang <makyl@newfang.io>
2022-07-11 15:46:49 +0400

    Converted auth to a singleton

--- src/lib/storageProvider.js
+++ src/lib/storageProvider.js
@@ -1,12 +1,15 @@
 import { StorageProvider } from '@arcana/storage'
 import { AuthProvider, AppMode } from '@arcana/auth'
 
+let auth
 let storageProvider
 
 export async function getWalletInstance () {
-  const wallet = new AuthProvider(process.env.REACT_APP_ARCANA_APP_ID)
-  await wallet.init({ appMode: AppMode.Full, position: 'right' })
-  return wallet.provider
+  if (!auth) {
+    auth = new AuthProvider(process.env.REACT_APP_ARCANA_APP_ID)
+    await auth.init({ appMode: AppMode.Full, position: 'right' })
+  }
+  return auth.provider
 }
 
 export default async function getStorageProvider (provider) {
